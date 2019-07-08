207997d8046629a4b3623d69fbcb98a71931cbf6
gstr169 <dmit.finn@yandex.ru>
2019-07-08 19:18:05 +0300

    Added postgres integration.

--- app/configs/settings.py
+++ app/configs/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': 'postgres',
+        'HOST': 'project_db',
+        'PORT': 5432,
     }
 }
 
