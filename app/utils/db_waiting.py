a925d1911885c71cf382cce0baf53cd354455a5e
gstr169 <dmit.finn@yandex.ru>
2020-01-15 23:16:04 +0300

    Improved db_waiting.py

--- app/utils/db_waiting.py
+++ app/utils/db_waiting.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 from time import sleep
+from configs.settings import DATABASES
 
 
 def wait_for_db(host: str, port: int, user: str, password: str, dbname: str, retries: int = 10, period: int = 2):
@@ -26,9 +27,11 @@ def wait_for_db(host: str, port: int, user: str, password: str, dbname: str, ret
 
 if __name__ == '__main__':
     # TODO: set postgres container name
-    host = 'project_db'
-    port = 5432
-    user = 'postgres'
-    password = os.getenv('PGPASSWORD')
-    dbname = os.getenv('PGDATABASE')
-    wait_for_db(host, port, user, password, dbname)
+    default_database = DATABASES.get('default')
+    if default_database:
+        host = default_database.get('HOST', 'project_db')
+        port = default_database.get('PORT', 5432)
+        user = default_database.get('USER', 'postgres')
+        password = os.getenv('PGPASSWORD')
+        dbname = os.getenv('PGDATABASE')
+        wait_for_db(host, port, user, password, dbname)
